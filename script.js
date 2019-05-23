cc92ef67ac68b6f317da1d5d709db2fd98265195
gravisanima <49438361+gravisanima@users.noreply.github.com>
2019-05-23 17:30:47 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-let sudokuLine = sudoku.generate('easy')
-console.log(sudokuLine)
+let sudokuLine = sudoku.generate('easy');
+console.log(sudokuLine);
 let iterator = 0;
 
+for(i = 0; i < 9; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.id = 's' + i;
+    newDiv.style = 'height:45px; width: 410px';
+    if ((i + 1) % 3 === 0) {
+        newDiv.style = 'border-bottom: 1px solid black; width: 410px; height:45px;'
+    }else if(i === 0){
+        newDiv.style = 'border-top: 1px solid black; width: 410px; height:45px;'
+    }
+    document.getElementById('main').appendChild(newDiv);
 
-for(i = 0; i < 9; i++){
-    let newDiv = document.createElement('div')
-    newDiv.id = 's'+i;
-    document.getElementById('main').appendChild(newDiv)
-
-    for(a = 0; a < 9; a++){
-        let newElement = document.createElement('button')
-        if(sudokuLine.charAt(iterator) === '.'){
+    for (a = 0; a < 9; a++) {
+        if(a === 0){
+            let newDiv1 = document.createElement('div');
+            newDiv1.style = 'height: 45px; width: 1px; background: black; display: inline-block;';
+            document.getElementById('s' + i).appendChild(newDiv1);
+        }
+        let newElement = document.createElement('button');
+        newElement.style = 'display: inline-block;';
+        if (sudokuLine.charAt(iterator) === '.') {
             newElement.innerHTML = ''
-        }else{
+        } else {
             newElement.innerHTML = sudokuLine.charAt(iterator)
         }
-        document.getElementById('s'+i).appendChild(newElement)
-        iterator++
+        document.getElementById('s' + i).appendChild(newElement);
+        iterator++;
+        if ((a + 1) % 3 === 0) {
+            let newDiv1 = document.createElement('div');
+            newDiv1.style = 'height: 45px; width: 1px; background: black; display: inline-block;';
+            document.getElementById('s' + i).appendChild(newDiv1);
+        }
     }
 }
+bootbox.prompt("Enter a number", function(result){
+    console.log(result);
+    });
